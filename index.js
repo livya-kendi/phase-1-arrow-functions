e264dcf829258b17f14bf7c578d03902895d90bf
Livya Kendi Kiruja <livyakendi1@gmail.com>
2022-06-16 18:38:26 +0300

    commit tree 288a0df8a49517ded5b3c9f3a9ae751c7a642302

---
+++ index.js
@@ -0,0 +1,19 @@
+function divide(){
+    const divide = (par1, par2); 
+    let par1 = 2000;
+    let par2= 100;
+    return par1 / par2;
+}
+
+function square() {
+  const multiply = (x) 
+  let x = 2;
+  return x*x;  
+}
+
+function add(){
+    const add = (a, b)
+    let a = 3
+    let b = 4
+    return a + b;
+}
